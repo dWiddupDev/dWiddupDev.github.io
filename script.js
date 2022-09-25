1eb95577ff42adbdc83fbefc542361dc051af61f
Danny <fatboy567@hotmail.co.uk>
2022-09-25 20:32:12 +0100

    donkey work begun

--- script.js
+++ script.js
@@ -13,11 +13,30 @@ function togglePanel(el) {
     }
 }
 
-function closeThisPanel(el) {
+function closeThisPanel(fishId) {
+    document.getElementById(`${fishId}_cont`).style.display = "none";
+    console.log("inner = ", fishId);
 
+    var sideListElSelected = document.querySelectorAll(".sidebar_selected");
+    var el;
+    for (var i = 0; i < sideListElSelected.length; i++) {
+        var selected = sideListElSelected[i].innerText.toLowerCase();
+        var cleanFish = fishId.replaceAll("_", " ");
+        if (selected == cleanFish) {
+            el = sideListElSelected[i];
+        }
+    }
+
+    var p = el.childNodes[1];
+    if (!el.classList.contains("sidebar_selected")) {
+        el.classList.add("sidebar_selected");
+        p.classList.replace("fa-caret-down", "fa-caret-right");
+        //el.className.replace("fa-caret-down", "fa-caret-right");
+    } else {
+        el.classList.remove("sidebar_selected");
+        p.classList.replace("fa-caret-right", "fa-caret-down");
+    }
 }
-
-
 
 // plan out how this will save properly
 function selectFish(el) {
@@ -35,8 +54,12 @@ function selectFish(el) {
     }
 
     // show selection options
+    var fishCont = document.getElementById(`${fishId}_cont`);
 
-    //something something - cont for the id
+    if (fishCont.style.display == "flex")
+        fishCont.style.display = "none";
+    else
+        fishCont.style.display = "flex";
 
 }
 
@@ -50,6 +73,33 @@ function addSelectionToLocal(fish, value) {
         case "salmon":
             selectionType = document.getElementById("salmon_type_select").value;
             fish = fish + "_" + selectionType;
+            break;
+        case "cod":
+            selectionType = document.getElementById("cod_type_select").value;
+            fish = fish + "_" + selectionType;
+            break;
+        case "hake":
+            selectionType = document.getElementById("hake_type_select").value;
+            fish = fish + "_" + selectionType;
+            break;
+        case "coley":
+            selectionType = document.getElementById("coley_type_select").value;
+            fish = fish + "_" + selectionType;
+            break;
+        case "seatrout":
+            selectionType = document.getElementById("seatrout_type_select").value;
+            fish = fish + "_" + selectionType;
+            break;
+        case "pollock":
+            selectionType = document.getElementById("pollock_type_select").value;
+            fish = fish + "_" + selectionType;
+            break;
+        case "wildbass":
+            selectionType = document.getElementById("wildbass_type_select").value;
+            fish = fish + "_" + selectionType;
+            break;
+        case "seaBass":
+            selectionType = "";
             break;
     }
 
@@ -83,7 +133,7 @@ function addPortionsToLocal(fish, value) {
 
 function addCustomToLocal(inputId) {
     var fish = inputId.split("_")[0];
-    var value = document.getElementById(`${inputId}`).value + " "; // add space so there it can be split
+    var value = document.getElementById(`${inputId}`).value; // add space so there it can be split
     addSelectionToLocal(fish, value);
     document.getElementById(`${inputId}`).value = "";
 }
